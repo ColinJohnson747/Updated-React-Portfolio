91975e21a19404d05256bbed1ca4f3bdd9c5455b
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-07-15 17:48:40 -0400

    init

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Colin Johnson",
+  role: " growing Full Stack Developer",
+  linkedinId: "colin-johnson-9250b974",
+  skypeid: "Your skypeid",
+  roleDescription:
+    " Ambitious and motivated, I am looking to become a driven and helpful member to any team.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/colin-johnson-9250b974",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/ColinJohnson747",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com/rbhatia46",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am a young and motivated developer, looking to grow my knowledge and skills. I have participated in rigorous and thorough training from the UNC Coding bootcamp, in addition to learing and practicing code in my own time. I am practiced in HTML, CSS, Javascript, Reactjs, and more. In addition to this I have experince working with different creation engines, such as Unreal 4 and Unity. I look forward to working with your team!",
+  address: "",
+  website: "https://rbhatia46.github.io",
+  education: [
+    {
+      UniversityName: "The University of North Carolina Coding Boot Camp",
+      specialization: "Certification in Fullstack Development",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2020",
+      Achievements: "Some Achievements",
+    },
+  ],
+  work: [],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Javascript",
+    },
+    {
+      skillname: "MongoDB",
+    },
+    {
+      skillname: "MySql",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Weather Forcaster",
+      description:
+        "A web app to look up the current weather in differnt locations",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
